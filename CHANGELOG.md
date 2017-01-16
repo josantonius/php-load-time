ffde03f57328f8b429982194e3347091830867b4
Josantonius <info@josantonius.com>
2017-01-17 00:44:25 +0100

    Initial upload files

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG
+
+## 1.0.0 - 2017-01-17
+* Added `Josantonius\LoadTime\LoadTime` class.
+* Added `Josantonius\LoadTime\LoadTime::start()` method.
+* Added `Josantonius\LoadTime\LoadTime::end()` method.
+
+## 1.0.0 - 2017-01-17
+* Added `Josantonius\LoadTime\Exception\LoadTimeException` class.
+* Added `Josantonius\LoadTime\Exception\Exceptions` abstract class.
+* Added `Josantonius\LoadTime\Exception\LoadTimeException->__construct()` method.
+
+## 1.0.0 - 2017-01-17
+* Added `Josantonius\LoadTime\Tests\LoadTimeTest` class.
+* Added `Josantonius\LoadTime\Tests\LoadTimeTest::testLoadTimeScript()` method.
---
+++ src/LoadTime.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+/**
+ * Calculate load time of pages or scripts.
+ * 
+ * @category   JST
+ * @package    LoadTime
+ * @subpackage LoadTime
+ * @author     Josantonius - info@josantonius.com
+ * @copyright  Copyright (c) 2017 JST PHP Framework
+ * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
+ * @version    1.0.0
+ * @link       https://github.com/Josantonius/PHP-LoadTime
+ * @since      File available since 1.0.0 - Update: 2017-01-17
+ */
+
+namespace Josantonius\LoadTime;
+
+# use Josantonius\LoadTime\Exception\LoadTimeException;
+
+/**
+ * Load time handler.
+ *
+ * @since 1.0.0
+ */
+class LoadTime {
+
+    /**
+     * Prefix for cookies.
+     *
+     * @since 1.0.0
+     *
+     * @var float
+     */
+    public static $startTime;
+
+    /**
+     * Set initial time.
+     *
+     * @since 1.0.0
+     *
+     * @return float → seconds
+     */
+    public static function start(): float {
+
+        return static::$startTime = microtime(true);
+    }
+
+    /**
+     * Set end time.
+     *
+     * @since 1.0.0
+     *
+     * @return float → seconds
+     */
+    public static function end(): float {
+
+        return round((microtime(true) - static::$startTime), 4);
+    }
+}
