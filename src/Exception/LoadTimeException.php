4a412fbe31a9fbc52d8304d1ba666c02bc1e6bc8
Josantonius <info@josantonius.com>
2017-01-30 02:52:02 +0100

    Repository version updated and the minimum required PHP version

--- src/Exception/LoadTimeException.php
+++ src/Exception/LoadTimeException.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Calculate load time of pages or scripts.
  * 
@@ -35,7 +35,7 @@ class LoadTimeException extends \Exception {
      * @param int    $error  → error code (Optional)
      * @param int    $status → HTTP response status code (Optional)
      */
-    public function __construct(string $msg = '', int $error = 0, int $status = 0) {
+    public function __construct($msg = '', $error = 0, $status = 0) {
 
         $this->message    = $msg;
         $this->code       = $error;
